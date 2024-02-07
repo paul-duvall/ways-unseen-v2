b434b632c2f277a628f0a114e55f125a9ff50dc8
Paul Duvall <paul.duvall@myfuturechoice.com>
2024-02-07 18:43:14 +0000

    Chore: Add primevue

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import PrimeVue from 'primevue/config'
+import Button from 'primevue/button'
+
+import 'primevue/resources/themes/lara-light-indigo/theme.css'
+import 'primevue/resources/primevue.min.css'
+
+
 const app = createApp(App)
 
 app.use(router)
-
+app.use(PrimeVue)
+app.component('AppButton', Button)
 app.mount('#app')
