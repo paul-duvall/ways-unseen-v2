d98334df9f64546b22df3ebaf5ebea3663bf42ec
Paul Duvall <paul.duvall@myfuturechoice.com>
2024-02-11 14:45:01 +0000

    feat: add main navigation

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,36 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import { RouterLink, RouterView } from 'vue-router'
+
+const items = ref([
+    {
+        label: 'Home',
+        icon: 'pi pi-home',
+        route: '/'
+    },
+    {
+        label: 'About',
+        icon: 'pi pi-star',
+        route: '/about'
+    },
+  ])
 </script>
 
 <template>
   <header>
     <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+      <AppMenubar :model="items">
+        <template #start>
+          <img src="./assets/ways-unseen-logo-smaller.png" width="40" />
+        </template>
+        <template #item="{ item, props }">
+                <router-link v-slot="{ href, navigate }" :to="item.route" custom>
+                    <a :href="href" v-bind="props.action" @click="navigate">
+                       <span>{{ item.label }}</span>
+                    </a>
+                </router-link>
+            </template>
+      </AppMenubar>
     </div>
   </header>
 
--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,9 @@ import router from './router'
 
 import PrimeVue from 'primevue/config'
 import Button from 'primevue/button'
+import Menubar from 'primevue/menubar'
+import Image from 'primevue/image';
+
 
 import 'primevue/resources/themes/lara-light-indigo/theme.css'
 import 'primevue/resources/primevue.min.css'
@@ -16,4 +19,6 @@ const app = createApp(App)
 app.use(router)
 app.use(PrimeVue)
 app.component('AppButton', Button)
+app.component('AppMenubar', Menubar)
+app.component('AppImage', Image)
 app.mount('#app')
