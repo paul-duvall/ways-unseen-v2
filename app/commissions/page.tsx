4122fc5e70a293b39ab82f0e8120094b959da19d
Paul Duvall <paul.duvall@myfuturechoice.com>
2024-12-31 11:25:36 +0000

    feat: improve commission page

--- app/commissions/page.tsx
+++ app/commissions/page.tsx
@@ -1,11 +1,11 @@
-import Image from "next/image"
+import styles from "./page.module.css"
 
 export default function Commissions() {
     return (
-      <div>
+      <div className={styles.page}>
         <main>
           <section className={ 'section-container' }>
-          <h1>Commissions</h1>
+            <h1>Commissions</h1>
             <p> 
                 Firstly, thanks for finding your way here and considering commissioning me to draw you 
                 a map!
@@ -16,9 +16,13 @@ export default function Commissions() {
                 styles I can use, check out my <a href="https://cara.app/waysunseen/portfolio" target="_blank">portfolio</a>!
             </p>
             <p>
+                If you'd like to get in contact you can email me at <a href="mailto:ways.unseen@gmail.com">ways.unseen@gmail.com</a> or 
+                you can message me on <a href="ways-unseen.bsky.social" target="_blank">Bluesky</a> or <a href="https://www.instagram.com/ways.unseen" target="_blank">Instagram</a>.
+            </p>
+            <p>
                 My general commission process is as follows: 
             </p>
-            <ol>
+            <ol className={styles['commission-stages']}>
                 <li>
                     You email me your requirements, including an written outline of what you would 
                     like me to draw. The more detail you provide, the more easily I can work towards 
