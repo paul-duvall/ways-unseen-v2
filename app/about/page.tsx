5dba284d25c02a4aafb5672f205e0934c7a33710
Paul Duvall <paul.duvall@myfuturechoice.com>
2025-01-05 11:25:00 +0000

    feat: update about and commissions text

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 // import Image from "next/image";
 // import styles from "./../page.module.css";
 
@@ -9,29 +10,37 @@ export default function About() {
       <main>
         <section className={ 'section-container' }>
         <h1>About Ways Unseen</h1>
-          <p>I&apos;m an illustrator and cartographer of sorts, living in the east of England in a county
+          <p>I&apos;m a fantasy map illustrator of sorts, living in the east of England in a county
           called Norfolk. I&apos;ve always enjoyed drawing, though for much of my life, I have shied away
           from actually putting pencil to paper. Now that I&apos;ve started in earnest, I never want 
           to stop!</p>
 
-          <p>I started out drawing top-down maps when I discovered solo tabletop gaming (games like Four
-          Against Darkness) and, instead of sketching out purely functional outlines, I found myself
-          compelled to take my time and make my maps beautiful.</p>
+          <p>I started out drawing top-down maps when I discovered solo tabletop gaming (games like <a href="https://www.drivethrurpg.com/en/product/180588/four-against-darkness" target="_blank">Four
+          Against Darkness</a> and <a href="https://www.drivethrurpg.com/en/product/308955/notequest" target="_blank">Notequest</a>) that
+          require simple dungeon maps to be drawn as you play that game. Instead of sketching out purely
+          functional outlines, I found myself compelled to take my time and make my maps beautiful.</p>
 
           <p>In the years since, I&apos;ve been creating and sharing maps in an increasingly broad range of
             styles. After starting with traditional top-down style maps, I progressed onto more
-            challenging isometric locations. These proved quite popular and saw my social media 
-            following grow quickly.
+            challenging isometric locations. These proved quite popular and my social media following grew
+            quickly.
           </p>
 
-          <p>This increased exposure lead to commission requests and I&apos;ve now worked with a number of
-            game designers to produce high-quality hand-drawn maps to help realise their creations. As
-            time has gone on, I&apos;ve broadened the range of map styles, including region and town maps, hex-maps
+          <p>This increased exposure led to commission requests and I&apos;ve now worked with a number of
+            clients to produce high-quality hand-drawn maps to help them realise their creations. I really
+            value these opportunities to create illustrations for a range of publications, not only because
+            it is lovely to see my maps in print and being enjoyed by people, but also as working on commissions
+            challenges me to draw subjects I might otherwise not attempt. The creativity and imagination of
+            my clients encourages me to broaden my range and produce illustrations of the highest
+            possible quality.
+          </p>
+
+          <p>
+            As time has gone on, I&apos;ve broadened the range of map styles, including region and town maps, hex-maps
             and cut away maps, each enabling me to represent locations in different ways.
           </p>
 
-          <p>Thanks for showing an interest in my work. (Add some contact info and links to other
-            sections of the site.
+          <p>Thanks for showing an interest in my work. You can find out more about my commission process <Link href="/commissions">here</Link>.
           </p>
         </section>
       </main>
--- app/commissions/page.tsx
+++ app/commissions/page.tsx
@@ -7,33 +7,35 @@ export default function Commissions() {
           <section className={ 'section-container' }>
             <h1>Commissions</h1>
             <p> 
-                Firstly, thanks for finding your way here and considering commissioning me to draw you 
-                a map!
+                If you&apos;ve found your way to this page, you&apos;re probably considering hiring me to draw you
+                a map. Thanks!
             </p>
             <p>
-                The illustrations I create for commissions are typically either isometric or top 
-                down maps, though I have created maps in other styles. If you&apos;re curious about the range of
-                styles I can use, check out my <a href="https://cara.app/waysunseen/portfolio" target="_blank">portfolio</a>!
+                The illustrations I create for commissions are typically either isometric or top-down maps, though
+                I have created maps in other styles. If you&apos;re curious about the range of styles I can use, 
+                check out my <a href="https://cara.app/waysunseen/portfolio" target="_blank">portfolio</a>!
             </p>
             <p>
                 If you would like to get in contact you can email me at <a href="mailto:ways.unseen@gmail.com">ways.unseen@gmail.com</a> or 
                 you can message me on <a href="ways-unseen.bsky.social" target="_blank">Bluesky</a> or <a href="https://www.instagram.com/ways.unseen" target="_blank">Instagram</a>.
             </p>
+            <h2>Process</h2>
             <p>
-                My general commission process is as follows: 
+                Here&apos;'s a brief overview of how I typically handle a commission to give you an idea of what to expect if
+                you hire me to draw a map: 
             </p>
             <ol className={styles['commission-stages']}>
                 <li>
-                    You email me your requirements, including an written outline of what you would 
-                    like me to draw. The more detail you provide, the more easily I can work towards 
-                    your vision. You can send me rough sketches or reference material - in fact, I 
-                    encourage you to. If you have spotted a drawing I have done you like, you can let me 
-                    know which one. 
+                    You email me as much information as you can, outlining your ideas. The more detail you provide, 
+                    the more effectively I can work towards your vision. You can send me rough sketches or reference material.
+                    If you have spotted an existing Ways Unseen illustration that you like and you think the style closely 
+                    matches how you imagine the commission looking, you can let me know which one. 
                     
                 </li>
                 <li>
-                    I will respond with any clarifying questions and will endeavor to give you a quote and 
-                    outline the process going forward. 
+                    We will work together to progress towards an agreed understanding of what form the final illustration will take.
+                    I will provide a quote and ensure you have full awareness of the process going forward, including agreeing a 
+                    timeframe for delivery. 
                 </li>
                 <li>
                     Once the work has been agreed, a 20% deposit is payable via Paypal (or full payment can be make 
@@ -44,12 +46,13 @@ export default function Commissions() {
                     is the stage at which you are able to request any changes to the layout. 
                 </li>
                 <li>
-                    If you are happy with the initial sketch, I will create the final illustration and send you a 
-                    final proof. 
+                    If you are happy with the initial sketch, I will draw the final illustration and send you a 
+                    final proof so you can see the illustration before paying the final amount. We can discuss the
+                    possibility of adjustments - some may be possible. 
                 </li>
                 <li>
-                    Once agreed, any outstanding final balance will be paid via Paypal. After receipt of the balance, 
-                    the final digital files will be provided. 
+                    Once you are happy with the final illustration, any outstanding final balance will be paid via Paypal, 
+                    and the final digital files will be provided. 
                 </li>
             </ol>
           </section>
