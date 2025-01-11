fbe3f639318d327e7b91fb7a71e6c1d45cebf183
Paul Duvall <duvallpj@gmail.com>
2025-01-11 11:42:07 +0000

    feat: add further fields to contact form

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,4 +1,5 @@
 "use client"
+import styles from "./page.module.css"
 
 export default function Contact() {
     const handleFormSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
@@ -18,15 +19,27 @@ export default function Contact() {
       };
 
     return (
-    // className={styles.page}
-    <div>
-        <main>
+    <div className={styles.page}>
+        <main className={ styles.contact }>
             <section className={ 'section-container' }>
-                <h1>Commissions</h1>
+                <h1>Contact</h1>
+                <p>
+                    If you want to discuss a project or make an enquiry about a commission,
+                    you can email me or fill in the form below.
+                </p>
+                <p>
+                    If you would like to get in contact you can email me at <a href="mailto:ways.unseen@gmail.com">ways.unseen@gmail.com</a> or 
+                    you can message me on <a href="ways-unseen.bsky.social" target="_blank">Bluesky</a> or <a href="https://www.instagram.com/ways.unseen" target="_blank">Instagram</a>. 
+                </p>
                 <form name="contact" onSubmit={handleFormSubmit}>
                     <input type="hidden" name="form-name" value="contact" />
-                    <input name="query" type="text" />
-                    <button type="submit">Search</button>
+                    <label for="name">Name:</label>
+                    <input id="name" name="name" type="text" required />
+                    <label for="email">Email:</label>
+                    <input id="email" name="email" type="email" required />
+                    <label for="message">Message:</label>
+                    <input id="message" name="message" type="text" required />
+                    <button type="submit">Submit</button>
                 </form>
             </section>
         </main>
