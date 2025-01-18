db3e86503642b6d01ce88add7f2f341156551de9
Paul Duvall <duvallpj@gmail.com>
2025-01-18 10:58:16 +0000

    feat: expand form functionality - clearing and success / error message

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,12 @@
 "use client"
+
+import { useRef, useState } from "react";
 import styles from "./page.module.css"
 
 export default function Contact() {
+    const formRef = useRef<HTMLFormElement>(null);
+    const [successMessage, setSuccessMessage] = useState<string>("")
+
     const handleFormSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
         const formData = new FormData(event.currentTarget);
@@ -10,12 +15,25 @@ export default function Contact() {
         formData.forEach((value, key) => {
         data[key] = value.toString();
 });
-        await fetch("/__forms.html", {
-          method: "POST",
-          headers: { "Content-Type": "application/x-www-form-urlencoded" },
-          body: new URLSearchParams(data).toString(),
-        });
-        // Success and error handling ...
+        try {
+            const response = await fetch("/__forms.html", {
+                method: "POST",
+                headers: { "Content-Type": "application/x-www-form-urlencoded" },
+                body: new URLSearchParams(data).toString(),
+              });
+
+            if (response.ok) {
+                setSuccessMessage("Your message has been successfully sent!");
+                formRef.current?.reset(); // Clear the form only if submission is successful
+            } else {
+                setSuccessMessage("Something went wrong. Please try again later.");
+            }
+        } catch {
+            setSuccessMessage("An error occurred. Please try again.");
+        }
+        
+        // Clear the form
+        formRef.current?.reset();
       };
 
     return (
@@ -31,7 +49,11 @@ export default function Contact() {
                     If you would like to get in contact you can email me at <a href="mailto:ways.unseen@gmail.com">ways.unseen@gmail.com</a> or 
                     you can message me on <a href="ways-unseen.bsky.social" target="_blank">Bluesky</a> or <a href="https://www.instagram.com/ways.unseen" target="_blank">Instagram</a>. 
                 </p>
-                <form name="contact" onSubmit={handleFormSubmit}>
+                <form
+                    ref={formRef}
+                    name="contact"
+                    onSubmit={handleFormSubmit}
+                >
                     <input type="hidden" name="form-name" value="contact" />
                     <div className={styles['contact-field']}>
                         <label htmlFor="name">Name</label>
@@ -49,6 +71,9 @@ export default function Contact() {
                         <button type="submit">Submit</button>
                     </div>          
                 </form>
+                {successMessage && (
+                        <p className={styles.successMessage}>{successMessage}</p>
+                    )}
             </section>
         </main>
     </div>
