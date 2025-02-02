65327a5089c8671b58d2a7e89afcdaab51858204
Paul Duvall <duvallpj@gmail.com>
2025-02-02 08:39:48 +0000

    feat: update navigation css

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,17 +5,17 @@ import Image from "next/image"
 export default function Navbar() {
   return (
   <nav>
-    <div>
-    <Image
-      src="/logo-grey.png"
-      alt="Banner image"
-      layout="intrinsic"
-      width={60}
-      height={60}
-    />
-    <span className={ 'navbar-logo-text' }>
-      <b><Link href="/">Ways Unseen</Link></b>
-    </span>
+    <div className={ 'navbar-logo-container' }>
+      <Image
+        src="/logo-grey.png"
+        alt="Banner image"
+        layout="intrinsic"
+        width={60}
+        height={60}
+      />
+      <span className={ 'navbar-logo-text' }>
+        <b><Link href="/">Ways Unseen</Link></b>
+      </span>
     </div>
     <div className={ 'navbar-links-container' }>
       <Link href="/">Home</Link>
